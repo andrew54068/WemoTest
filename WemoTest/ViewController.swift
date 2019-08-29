3aeec956b095816f624a05bd827d433303b8f8b9
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-08-29 16:10:25 +0800

    Initial Commit

---
+++ WemoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WemoTest
+//
+//  Created by kidnapper on 2019/8/29.
+//  Copyright © 2019 andrew54068. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
